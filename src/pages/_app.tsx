b0379f98a3e848786398b5dcd9ddc6e453f7da98
Kyaw Naing Moe <kyawnaingmoe.dev@gmail.com>
2023-05-12 22:34:39 +0630

    vercel build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import Layout from "../layout/Layout";
+
+//export default function App({ Component, pageProps }: AppProps) {
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  //return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
 }
